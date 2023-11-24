a7b92283e6314128a438d22cfc6ee8c3956c4600
SosbI4 <sds@fsla.fd>
2023-11-24 12:44:40 +0300

    Создание компании тест

---
+++ src/test/java/com/sosoburger/careerguide/CompanyControllerTest.java
@@ -0,0 +1,83 @@
+package com.sosoburger.careerguide;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sosoburger.careerguide.dto.request.RequestCompanyDTO;
+import com.sosoburger.careerguide.dto.response.ResponseCompanyDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Sql(value = "classpath:data.sql")
+class CompanyControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper ob = new ObjectMapper();
+
+    @Test
+    @SqlGroup({
+            @Sql(value = "classpath:reset.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(value = "classpath:data.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(value = "classpath:reset.sql", executionPhase = BEFORE_TEST_METHOD)
+    })
+    @WithMockUser(username = "company", password = "company", roles = "COMPANY")
+    void createCompany() {
+        UserControllerTest.CreateUser(mockMvc, "company", "company", "COMPANY");
+        try {
+            RequestCompanyDTO companyDTO = new RequestCompanyDTO(
+                    "Компания",
+                    "г. Пенза, ул. Гачи 1337",
+                    "Очень хорошее предприятие",
+                    "894343534",
+                    "company@gmai;.cum",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1025px-Cat03.jpg");
+
+            mockMvc.perform(
+                            post("/company")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(ob.writeValueAsBytes(companyDTO))
+                                    .param("login", "company")
+                    ).andExpect(status().isCreated())
+                    .andDo(result -> {
+                        var response = ob.readValue(result.getResponse().getContentAsByteArray(), ResponseCompanyDTO.class);
+                        assert (response.getCompanyId() == 1);
+                        assert (response.getCompanyName().equals(companyDTO.getCompanyName()));
+                        assert (response.getImage().equals(companyDTO.getImage()));
+                        assert (response.getEmail().equals(companyDTO.getEmail()));
+                        assert (response.getAddress().equals(companyDTO.getAddress()));
+                        assert (response.getDescription().equals(companyDTO.getDescription()));
+                        assert (response.getPhone().equals(companyDTO.getPhone()));
+                    })
+            ;
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    void updateCompany() {
+    }
+
+    @Test
+    void getCompany() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
--- src/test/java/com/sosoburger/careerguide/UserControllerTest.java
+++ src/test/java/com/sosoburger/careerguide/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,13 +18,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerTest {
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-    ObjectMapper ob = new ObjectMapper();
+    private static final ObjectMapper ob = new ObjectMapper();
+
+
+    public static void CreateUser(MockMvc mockMvc,String login, String password, String role){
+        try {
+            mockMvc.perform(
+                    post("/user/signup")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(ob.writeValueAsString(new RegDTO(login, password, role)))
+            );
+            System.out.println("Пользователь " + login + " создан");
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
     @Test
-    @Sql(value = "classpath:resetUser.sql", executionPhase = BEFORE_TEST_METHOD)
+    @Sql(value = "classpath:reset.sql", executionPhase = BEFORE_TEST_METHOD)
     void registerUser() {
         try {
             RegDTO user = new RegDTO("company", "company", "COMPANY");
