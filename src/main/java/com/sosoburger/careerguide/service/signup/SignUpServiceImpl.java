496aa518382526f7b866c3b7ed63d7fca498f5d6
SosbI4 <sds@fsla.fd>
2023-11-30 14:52:58 +0300

    Теперь приходит на почту

--- src/main/java/com/sosoburger/careerguide/service/signup/SignUpServiceImpl.java
+++ src/main/java/com/sosoburger/careerguide/service/signup/SignUpServiceImpl.java
@@ -9,17 +9,15 @@ import com.sosoburger.careerguide.service.file.FileService;
 import com.sosoburger.careerguide.service.institution.InstitutionService;
 import com.sosoburger.careerguide.service.schedule.ScheduleService;
 import jakarta.mail.Message;
-import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.util.ByteArrayDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +37,8 @@ public class SignUpServiceImpl implements SignUpService {
     private CompanyService companyService;
     @Autowired
     private JavaMailSender emailSender;
+
+    SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm");
 
     @Override
     public SignUpDAO save(RequestSignUpDTO signUpDTO) {
@@ -66,8 +66,10 @@ public class SignUpServiceImpl implements SignUpService {
         try {
             SignUpDAO signUpDAO = signUpDTO.toDAO();
             signUpDAO.setSignUpId(savedSignUp.getSignUpId());
-            signUpDAO.setSchedule(savedSignUp.getSchedule());
-            signUpDAO.setInstitution(savedSignUp.getInstitution());
+            signUpDAO.setSchedule(scheduleService.get(signUpDTO.getSchedule()));
+            signUpDAO.setInstitution(institutionService.get(signUpDTO.getInstitution()));
+            signUpDAO.setFile(fileService.get(signUpDTO.getFile()
+            ));
             return signUpRepository.save(signUpDAO);
         } catch (ParseException e) {
             throw new RuntimeException(e);
@@ -98,14 +100,23 @@ public class SignUpServiceImpl implements SignUpService {
     public void changeStatus(Integer id, Boolean status) {
         SignUpDAO signUpDAO = get(id);
         signUpDAO.setStatus(status);
-        if (status){
+        if (status) {
             MimeMessagePreparator preparator = message -> {
                 message.setFrom("nik.isaev2004@mail.ru");
                 message.setRecipients(Message.RecipientType.TO, signUpDAO.getSchedule().getCompany().getEmail());
                 message.setSubject("Оповещение!");
-                message.setText("Вы приняли заявку на экскурсию: " + signUpDAO.getDate() + "К вам придет " + signUpDAO.getCount() + "Человек");
-                MimeMessageHelper helper = new MimeMessageHelper(message);
-                helper.addAttachment(signUpDAO.getFile().getName(), new ByteArrayDataSource(signUpDAO.getFile().getData(),signUpDAO.getFile().getType()));
+                MimeMessageHelper helper = new MimeMessageHelper(message, true);
+                helper.addAttachment(signUpDAO.getFile().getName(),
+                        new ByteArrayDataSource(signUpDAO.getFile().getData(), signUpDAO.getFile().getType()));
+                helper.setText("Вы приняли заявку на экскурсию: " +
+                        formatter.format(signUpDAO
+                                .getSchedule()
+                                .getDate()
+                                .getTime())
+                        +
+                        ".\nК вам придет " +
+                        signUpDAO.getCount() +
+                        " человек");
 
             };
             emailSender.send(preparator);
