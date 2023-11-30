6a95a2c6fa52862d4d7e8121ba564699509bba60
samui <nik.isaev2004@mail.ru>
2023-11-30 13:20:25 +0300

    Логин вынесен из класса в отдельны

--- src/main/java/com/sosoburger/careerguide/service/signup/SignUpServiceImpl.java
+++ src/main/java/com/sosoburger/careerguide/service/signup/SignUpServiceImpl.java
@@ -8,11 +8,20 @@ import com.sosoburger.careerguide.service.company.CompanyService;
 import com.sosoburger.careerguide.service.file.FileService;
 import com.sosoburger.careerguide.service.institution.InstitutionService;
 import com.sosoburger.careerguide.service.schedule.ScheduleService;
+import jakarta.mail.Message;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.util.ByteArrayDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SignUpServiceImpl implements SignUpService {
@@ -28,6 +37,8 @@ public class SignUpServiceImpl implements SignUpService {
 
     @Autowired
     private CompanyService companyService;
+    @Autowired
+    private JavaMailSender emailSender;
 
     @Override
     public SignUpDAO save(RequestSignUpDTO signUpDTO) {
@@ -87,6 +98,18 @@ public class SignUpServiceImpl implements SignUpService {
     public void changeStatus(Integer id, Boolean status) {
         SignUpDAO signUpDAO = get(id);
         signUpDAO.setStatus(status);
+        if (status){
+            MimeMessagePreparator preparator = message -> {
+                message.setFrom("nik.isaev2004@mail.ru");
+                message.setRecipients(Message.RecipientType.TO, signUpDAO.getSchedule().getCompany().getEmail());
+                message.setSubject("Оповещение!");
+                message.setText("Вы приняли заявку на экскурсию: " + signUpDAO.getDate() + "К вам придет " + signUpDAO.getCount() + "Человек");
+                MimeMessageHelper helper = new MimeMessageHelper(message);
+                helper.addAttachment(signUpDAO.getFile().getName(), new ByteArrayDataSource(signUpDAO.getFile().getData(),signUpDAO.getFile().getType()));
+
+            };
+            emailSender.send(preparator);
+        }
         signUpRepository.save(signUpDAO);
     }
 
